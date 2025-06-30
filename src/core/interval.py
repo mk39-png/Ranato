1c217c5e7d1e78f0f85f647fe04f7216f6b52ca6
mk39-png <113801254+mk39-png@users.noreply.github.com>
2025-06-30 12:37:19 -0500

    Change up interval for proper private members and readability

--- src/core/interval.py
+++ src/core/interval.py
@@ -1,75 +1,225 @@
+"""@package docstring
+    Representation of an interval in R that can be bounded or unbounded
+    and closed or open on both ends. Also supports sampling and predicates such
+    as compactness and containment.
+"""
 
-# @brief Representation of an interval in R that can be bounded or unbounded
-# and closed or open on both ends. Also supports sampling and predicates such
-# as compactness and containment.
+
 class Interval:
-    # ===================
-    # Private Members
-    # ===================
-    __m_t0: float
-    __m_t1: float
-    __m_bounded_below: bool
-    __m_bounded_above: bool
-    __m_open_below: bool
-    __m_open_above: bool
+    def __init__(self, lower_bound: float = -float("inf"), upper_bound: float = float("inf")):
+        """Default constructor. Which also has support for constructor where user passes in lower_bound and upper_bound"""
 
-    def __init__(self):
-        self.reset_bounds()
+        # *****************
+        # Protected Members
+        # *****************
+        self.m_t0: float = lower_bound
+        self.m_t1: float = upper_bound
+        self.m_bounded_below: bool = False
+        self.m_bounded_above: bool = False
+        self.m_open_below: bool = True
+        self.m_open_above: bool = True
 
-    def __init__(self, lower_bound: float, upper_bound: float):
-        set_lower_bound(lower_bound)
-        set_upper_bound(upper_bound)
+        # TODO: do I really need reset_bounds after everything above?
+        # self.reset_bounds()
 
-    def set_lower_bound(lower_bound: float,  is_open: bool = False):
+    # NOTE: I'm including these getters and setters becuase they have more logic beyond simple getting and setting
+    def set_lower_bound(self, lower_bound: float,  is_open: bool = False) -> None:
+        self.m_t0 = lower_bound
+        self.m_bounded_below = True
+        self.m_open_below = is_open
 
-    def set_upper_bound(upper_bound: float, is_open: bool = False):
+    def set_upper_bound(self, upper_bound: float, is_open: bool = False) -> None:
+        self.m_t1 = upper_bound
+        self.m_bounded_above = True
+        self.m_open_above = is_open
 
-    def trim_lower_bound(trim_amount: float):
-    def trim_upper_bound(trim_amount: float):
-    def pad_lower_bound(pad_amount: float):
-    def pad_upper_bound(pad_amount: float):
+    def trim_lower_bound(self, trim_amount: float) -> None:
+        # Don't trim if the interval is not bounded or has length of order trim
+        # amount
+        if ((not self.m_bounded_below) or (2.0 * abs(trim_amount) > self.get_length())):
+            return
+        self.m_t0 += trim_amount
 
-    @property
-    def get_lower_bound() -> float:
-        return m_t0
+    def trim_upper_bound(self, trim_amount: float) -> None:
+        # Don't trim if the interval is not bounded or has length of order trim
+        # amount
+        if ((not self.m_bounded_above) or (2.0 * abs(trim_amount) > self.get_length())):
+            return
+        self.m_t1 -= trim_amount
 
-    def get_upper_bound() -> float:
-    def get_center() -> float:
-    def get_length() -> float:
+    def pad_lower_bound(self, pad_amount: float) -> None:
+        #  Don't trim if the interval is not bounded or has length of order trim
+        #  amount
+        if ((not self.m_bounded_below) or (pad_amount < 0)):
+            return
+        self.m_t0 -= pad_amount
 
-    @property
-    bool is_bounded_below() const
-
-    @property
-    bool is_bounded_above() const
-
-    @property
-    bool is_open_below() const
-
-    @property
-    bool is_open_above() const
-
-    @property
-    bool is_finite() const
-
-    @property
-    bool is_compact() const
+    def pad_upper_bound(self, pad_amount: float) -> None:
+        # Don't trim if the interval is not bounded or has length of order trim
+        # amount
+        if ((not self.m_bounded_above) or (pad_amount < 0)):
+            return
+        self.m_t1 += pad_amount
 
     # TODO: float("inf") may be wrong...
     def reset_bounds(self) -> None:
-        m_t0 = -float("inf")
-        m_t1 = float("inf")
-        m_bounded_below = False
-        m_bounded_above = False
-        m_open_below = True
-        m_open_above = True
+        self.m_t0 = -float("inf")
+        self.m_t1 = float("inf")
+        self.m_bounded_below = False
+        self.m_bounded_above = False
+        self.m_open_below = True
+        self.m_open_above = True
 
-    @property
-    bool contains(self, t: float) const
+    def is_finite(self) -> bool:
+        return (self.m_bounded_above and self.m_bounded_below)
 
-    @property
-    bool is_in_interior(double t) const
+    def is_compact(self) -> bool:
+        return (self.is_finite() and (not self.m_open_above) and (not self.m_open_below))
 
-    std:: vector < double > sample_points(int num_points) const
+    def get_center(self) -> float:
+        return 0.5 * (self.m_t0 + self.m_t1)
 
-    std: : string formatted_interval() const
+    def get_length(self) -> float:
+        if (self.is_finite()):
+            # upper_bound - lower_bound
+            return self.m_t1 - self.m_t0
+        else:
+            return float("inf")
+
+    def contains(self, t: float) -> bool:
+        if ((not self.m_bounded_below) and (not self.m_bounded_above)):
+            return True
+
+        if ((not self.m_bounded_below) and (t <= self.m_t1)):
+            return True
+
+        if ((not self.m_bounded_above) and (t >= self.m_t0)):
+            return True
+
+        if ((t < self.m_t0) and (not self.m_open_below)):
+            return False
+
+        if ((t <= self.m_t0) and (self.m_open_below)):
+            return False
+
+        if ((t > self.m_t1) and (not self.m_open_above)):
+            return False
+
+        if ((t >= self.m_t1) and (self.m_open_above)):
+            return False
+
+        return True
+
+    def is_in_interior(self, t: float) -> bool:
+        if t <= self.m_t0:
+            return False
+        if t >= self.m_t1:
+            return False
+        return True
+
+    def sample_points(self, num_points: int) -> list[float]:
+        points = []
+
+        if num_points <= 1:
+            return points
+
+        # Unbounded below and above
+        if not self.is_bounded_above() and not self.is_bounded_below():
+            for i in range(num_points):
+                points.append(-num_points / 20.0 + 0.1 * i)
+            return points
+
+        # Unbounded below, closed above
+        elif not self.is_bounded_below() and not self.is_open_above():
+            upper = self.get_upper_bound()
+            for i in range(num_points):
+                points.append(upper - 0.1 * i)
+            return points
+
+        # Unbounded below, open above
+        elif not self.is_bounded_below() and self.is_open_above():
+            upper = self.get_upper_bound()
+            for i in range(1, num_points + 1):
+                points.append(upper - 0.1 * i)
+            return points
+
+        # Unbounded above, closed below
+        elif not self.is_bounded_above() and not self.is_open_below():
+            lower = self.get_lower_bound()
+            for i in range(num_points):
+                points.append(lower + 0.1 * i)
+            return points
+
+        # Unbounded above, open below
+        elif not self.is_bounded_above() and self.is_open_below():
+            lower = self.get_lower_bound()
+            for i in range(1, num_points + 1):
+                points.append(lower + 0.1 * i)
+            return points
+
+        # Bounded cases
+        t0 = self.get_lower_bound()
+        t1 = self.get_upper_bound()
+
+        # Closed interval [t0, t1]
+        if not self.is_open_below() and not self.is_open_above():
+            for i in range(num_points):
+                s = i / (num_points - 1)
+                t = (1.0 - s) * t0 + s * t1
+                points.append(t)
+
+        # Open below, closed above ( (t0, t1] )
+        elif self.is_open_below() and not self.is_open_above():
+            for i in range(1, num_points + 1):
+                s = i / num_points
+                t = (1.0 - s) * t0 + s * t1
+                assert t != t0
+                points.append(t)
+
+        # Closed below, open above [t0, t1)
+        elif not self.is_open_below() and self.is_open_above():
+            for i in range(num_points):
+                s = i / num_points
+                t = (1.0 - s) * t0 + s * t1
+                assert t != t1
+                points.append(t)
+
+        # Open interval (t0, t1)
+        elif self.is_open_below() and self.is_open_above():
+            for i in range(1, num_points + 1):
+                s = i / (num_points + 1)
+                t = (1.0 - s) * t0 + s * t1
+                points.append(t)
+
+        return points
+
+    # TODO: maybe add proper getters and setters for m_t0 and m_t1 for readability.
+
+    def __repr__(self):
+        interval_string = ""
+        # Unbounded cases
+        if (not self.m_bounded_above) and (not self.m_bounded_below):
+            interval_string += "(-infty, infty)"
+        elif (not self.m_bounded_below) and (not self.m_open_above):
+            interval_string += f"(-infty, {self.m_t1:.17g}]"
+        elif (not self.m_bounded_below) and self.m_open_above:
+            return f"(-infty, {self.m_t1:.17g})"
+        elif (not self.m_bounded_above) and (not self.m_open_below):
+            return f"[{self.m_t0:.17g}, infty)"
+        elif (not self.m_bounded_above) and self.m_open_below:
+            return f"({self.m_t0:.17g}, infty)"
+
+        # Bounded cases
+        t0: float = self.m_t0
+        t1: float = self.m_t1
+
+        if (not self.m_open_below) and (not self.m_open_above):
+            interval_string += f"[{t0:.17g}, {t1:.17g}]"
+        elif self.m_open_below and (not self.m_open_above):
+            interval_string += f"({t0:.17g}, {t1:.17g}]"
+        elif (not self.m_open_below) and self.m_open_above:
+            interval_string += f"[{t0:.17g}, {t1:.17g})"
+        elif self.m_open_below and self.m_open_above:
+            interval_string += f"({t0:.17g}, {t1:.17g})"
+
+        return interval_string
--- src/core/rational_function.py
+++ src/core/rational_function.py
@@ -1,27 +1,202 @@
+"""@package docstring
+    Quotient of scalar or vector valued polynomial functions over an interval.
+"""
+
+from dataclasses import dataclass
 import numpy as np
+from interval import Interval
+
+
+@dataclass
+class CurveDiscretizationParameters:
+    num_samples: int = 5
+    num_tangents_per_segment: int = 5
 
 
 class RationalFunction:
     # NOTE: degree and dimension ALWAYS passed into RationalFunction upon construction...
-    def __init__(self, degree, dimension):
+    # ************
+    # Constructors
+    # ************
+
+    # TODO: maybe have the arguments have default values... that are then dependent on degree and dimension...
+    # TODO: so that would entail some sort of lambda function?
+    def __init__(self, degree: int, dimension: int, numerator_coeffs: np.ndarray, denominator_coeffs: np.ndarray, domain: Interval):
+        """Default constructor"""
         # ****************
         # Member variables
         # ****************
-        # Instead of Eigen matrix... now using numpy matrices.
         # TODO: double check that the shape is as it should be with the Eigen matrix.
-        self.m_numerator_coeffs = np.ndarray(
-            shape=(degree+1, dimension), dtype='float64')
-        # Type interval variable...
+        self.m_degree = degree
+        self.m_dimension = dimension
+        self.m_numerator_coeffs = numerator_coeffs
+        self.m_denominator_coeffs = denominator_coeffs
+        self.m_domain = domain
+        assert (self.is_valid())
 
     @classmethod
-    # TODO: maybe swap out cls for a better name
-    def from_polynomial_function():
+    def from_zero_function(cls, degree: int, dimension: int):
+        """Default numerator set to constant in R^n"""
+        numerator_coeffs = np.zeros(
+            shape=(degree+1, dimension), dtype='float64')
+        denominator_coeffs = np.zeros(
+            shape=(degree+1, 1), dtype='float64')
+        denominator_coeffs[0] = 1.0
+        domain = Interval()
+        domain.reset_bounds()
 
-    def __init__(self, degree: int, dimension: int):
-        self.degree: int = degree
-        self.dimension: int = dimension
+        return cls(degree, dimension, numerator_coeffs, denominator_coeffs, domain)
 
-    #
-    # Constructors
-    #
-    # Default constructor for 0 function
+    @classmethod
+    def from_polynomial_function(cls, degree: int, dimension: int, numerator_coeffs: np.ndarray):
+        """ Constructor for a vector polynomial
+
+        Args:
+            numerator_coeffs (np.ndarray): coefficients of the polynomial functions
+
+        Return:
+            TODO: fill in the return value
+        """
+        denominator_coeffs = np.zeros(
+            shape=(degree+1, 1), dtype='float64')
+        denominator_coeffs[0] = 1.0
+        domain = Interval()
+        domain.reset_bounds()
+
+        return cls(degree, dimension, numerator_coeffs, denominator_coeffs, domain)
+
+    @classmethod
+    def from_real_line(cls, degree: int, dimension: int, numerator_coeffs: np.ndarray, denominator_coeffs: np.ndarray):
+        # TODO: assert the shape for numerator_coeffs and denominator_coeffs
+        """ General constructor over entire real line.
+
+        Args:
+            numerator_coeffs [in] (np.ndarray): coefficients of the numerator polynomial
+            denominator_coeffs [in] (np.ndarray): coefficients of the denominator polynomial
+
+        Return:
+            TODO: fill in return value
+        """
+        domain = Interval()
+        domain.reset_bounds()
+
+        return cls(degree, dimension, numerator_coeffs, denominator_coeffs, domain)
+
+    @classmethod
+    def from_interval(cls, degree: int, dimension: int, numerator_coeffs: np.ndarray, denominator_coeffs: np.ndarray, domain: Interval):
+        # TODO: assert the shape for numerator_coeffs and denominator_coeffs
+        """ General constructor over given interval.
+
+        Args:
+            numerator_coeffs [in] (np.ndarray): coefficients of the numerator polynomial
+            denominator_coeffs [in] (np.ndarray): coefficients of the denominator polynomial
+            domain [in] (Interval): domain interval for the mapping
+
+        Return:
+            TODO: fill in return value
+        """
+
+        return cls(degree, dimension, numerator_coeffs, denominator_coeffs, domain)
+
+    # *******
+    # Methods
+    # *******
+    # NOTE: this is never used in the ASOC code
+    @property
+    def degree(self):
+        pass
+
+    # NOTE: this is never used in the ASOC code
+    def get_dimension(self):
+        pass
+
+    def compute_derivative(self):
+        pass
+
+    def apply_one_form(self):
+        pass
+
+    def split_at_knot(self):
+        pass
+
+    def sample_points(self):
+        pass
+
+    def start_point(self):
+        pass
+
+    def mid_point(self):
+        pass
+
+    def end_point(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def evaluate_normalized_coordinate(self):
+        pass
+
+    def is_in_domain(self):
+        pass
+
+    def is_in_domain_interior(self):
+        pass
+
+    def discretize(self):
+        pass
+
+    # TODO: this is where I need to interact with the Blender API since *that* is now my viewer.
+    def add_curve_to_viewer(self):
+        pass
+
+    def finite_difference_derivative(self):
+        pass
+
+    # *******************
+    # Getters and setters
+    # *******************
+    def set_numerators(self):
+        pass
+
+    def set_denominator(self):
+        pass
+
+    def get_numerators(self):
+        pass
+
+    def get_denominator(self):
+        pass
+
+    # TODO: then have the domain accessible.
+    # TODO: do equivalent to "friend class Conic;"
+    def __is_valid(self):
+        # Checks columns of m_numerator_coeffs
+        if (self.m_numerator_coeffs.shape[1] == 0):
+            return False
+        if (self.m_denominator_coeffs.size == 0):
+            return False
+
+        return True
+
+    # ******************************
+    # Helper functions for operators
+    # ******************************
+
+    # TODO: turn "formatted_rational_function" into a __repr__ for when the rational function is printed in the interpreter
+    # TODO: finish a lot of these things for PolynomialFunction and Interval classes
+    def __repr__(self):
+        rational_function_string: str = "RationalFunction 1/()"
+        # rational_function_string += formatted_polynomial < degree, 1 > (
+        #     m_denominator_coeffs, 17)
+        rational_function_string += ") [\n  "
+
+        # for each column in m_numerator_coeffs
+        for i in range(self.m_numerator_coeffs.shape[1]):
+            # rational_function_string += formatted_polynomial < degree, 1 > (
+            #     m_numerator_coeffs.col(i), 17)
+            rational_function_string += ",\n  "
+
+        # rational_function_string += "], t in " + self.m_domain.formatted_interval()
+
+        return rational_function_string
