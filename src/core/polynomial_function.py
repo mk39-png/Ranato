2e010a29eeee9a33f6cd89b82529970e1cbf4e8a
mk39-png <113801254+mk39-png@users.noreply.github.com>
2025-07-07 10:33:27 -0500

    Changing folders around for testing + passing Polynomial Functions tests

--- src/core/polynomial_function.py
+++ src/core/polynomial_function.py
@@ -13,9 +13,28 @@ vector or matrix.
 
 # TODO: only import the things needed from numpy... that being ndarray.
 import numpy as np
-
+from ..core.common import *
+import logging
 
 # TODO: decide between SciPy and NumPy for implementing polynomials...
+logger = logging.getLogger(__name__)
+
+
+def remove_polynomial_trailing_coefficients(A_coeffs: np.ndarray):
+    """ Remove any zeros at the end of the polynomial coefficient vector
+
+    Args:
+        A_coeffs [in]
+        reduced_coeffs [out]
+
+    Return: 
+        reference to trimmed A_coeffs with trailing zeros removed
+    """
+
+    # TODO: assert that A_coeffs is indeed a vector and NOT a matrix.
+    assert A_coeffs.ndim == 1
+    # assert reduced_coeffs.ndim == 1
+    return np.trim_zeros(A_coeffs, 'b')
 
 
 def generate_monomials(degree: int, t: float, T: np.ndarray) -> None:
@@ -68,14 +87,14 @@ def evaluate_polynomial_mapping(degree: int, dimension: int, polynomial_coeffs: 
     pass
 
 
-def compute_polynomial_mapping_product(dimension: int, first_degree: int, second_degree: int, first_polynomial_coeffs: np.ndarray, second_polynomial_coeffs: np.ndarray, product_polynomial_coeffs: np.ndarray) -> None:
+def compute_polynomial_mapping_product(first_degree: int, second_degree: int, dimension: int, first_polynomial_coeffs: np.ndarray, second_polynomial_coeffs: np.ndarray, product_polynomial_coeffs: np.ndarray) -> None:
     """ Generate the polynomial coefficients for the kronecker product of two
         polynomials of the same dimension.
 
     Args:
-        dimension: polynomial dimension
         first_degree: maximum monomial degree of the first polynomial
         second_degree: maximum monomial degree of the second polynomial
+        dimension: polynomial dimension
         first_polynomial_coeffs [in]: coefficients of the first polynomial
         second_polynomial_coeffs [in]: coefficients of the second polynomial
         product_polynomial_coeffs [out]: product polynomial coefficients
@@ -84,11 +103,23 @@ def compute_polynomial_mapping_product(dimension: int, first_degree: int, second
         None
 
     """
-    assert np.shape(first_polynomial_coeffs) == (first_degree + 1, dimension)
-    assert np.shape(second_polynomial_coeffs) == (second_degree + 1, dimension)
-    assert np.shape(product_polynomial_coeffs) == (
+    # TODO: ask about the shape b/c originally I had something like the pseudocode below
+    # assert first_polynomial_coeffs.shape == (first_degree + 1, dimension) OR (first_degree + 1,)
+    assert first_polynomial_coeffs.shape == (first_degree + 1, dimension)
+    assert second_polynomial_coeffs.shape == (second_degree + 1, dimension)
+    assert product_polynomial_coeffs.shape == (
         first_degree + second_degree + 1, dimension)
-    pass
+
+    # Compute the new polynomial coefficients by convolution
+    # NOTE: must set all elements of original NumPy array to 0 rather than creating .zeros_like because Python handles references differently from C++
+    product_polynomial_coeffs[:, :] = 0
+
+    # XXX: This differs from C++ code in that it is not <= first_degree...
+    for i in range(first_degree+1):
+        for j in range(second_degree+1):
+            for k in range(dimension):
+                product_polynomial_coeffs[i + j, k] += first_polynomial_coeffs[i,
+                                                                               k] * second_polynomial_coeffs[j, k]
 
 
 def compute_polynomial_mapping_scalar_product(dimension: int, first_degree: int, second_degree: int, scalar_polynomial_coeffs: np.ndarray, polynomial_coeffs: np.ndarray, product_polynomial_coeffs: np.ndarray) -> None:
@@ -114,7 +145,7 @@ def compute_polynomial_mapping_scalar_product(dimension: int, first_degree: int,
     pass
 
 
-def compute_polynomial_mapping_cross_product(first_degree: int, second_degree: int, scalar_polynomial_coeffs: np.ndarray, polynomial_coeffs: np.ndarray, product_polynomial_coeffs: np.ndarray):
+def compute_polynomial_mapping_cross_product(first_degree: int, second_degree: int, first_polynomial_coeffs: np.ndarray, second_polynomial_coeffs: np.ndarray, product_polynomial_coeffs: np.ndarray):
     """ Generate the polynomial coefficients for the cross product of two
         vector valued polynomial mappings with range R^3.
 
@@ -125,11 +156,39 @@ def compute_polynomial_mapping_cross_product(first_degree: int, second_degree: i
     product_polynomial_coeffs [out]: product vector valued polynomial
     mapping coefficients
     """
-    assert np.shape(scalar_polynomial_coeffs) == (first_degree + 1, 3)
-    assert np.shape(polynomial_coeffs) == (second_degree + 1, 3)
+    assert np.shape(first_polynomial_coeffs) == (first_degree + 1, 3)
+    assert np.shape(second_polynomial_coeffs) == (second_degree + 1, 3)
     assert np.shape(product_polynomial_coeffs) == (
         first_degree + second_degree + 1, 3)
-    pass
+
+    shape = (first_degree + second_degree + 1, 1)
+
+    A0B1 = np.ndarray(shape)
+    A0B2 = np.ndarray(shape)
+    A1B0 = np.ndarray(shape)
+    A1B2 = np.ndarray(shape)
+    A2B0 = np.ndarray(shape)
+    A2B1 = np.ndarray(shape)
+
+    # Below lines of code retrieving particular columns of first_polynomial_coeffs and second_polynomial_coeffs
+    compute_polynomial_mapping_product(first_degree, second_degree, 1,
+                                       first_polynomial_coeffs[:, 0].reshape(-1, 1), second_polynomial_coeffs[:, 1].reshape(-1, 1), A0B1)
+    compute_polynomial_mapping_product(first_degree, second_degree, 1,
+                                       first_polynomial_coeffs[:, 0].reshape(-1, 1), second_polynomial_coeffs[:, 2].reshape(-1, 1), A0B2)
+    compute_polynomial_mapping_product(first_degree, second_degree, 1,
+                                       first_polynomial_coeffs[:, 1].reshape(-1, 1), second_polynomial_coeffs[:, 0].reshape(-1, 1), A1B0)
+    compute_polynomial_mapping_product(first_degree, second_degree, 1,
+                                       first_polynomial_coeffs[:, 1].reshape(-1, 1), second_polynomial_coeffs[:, 2].reshape(-1, 1), A1B2)
+    compute_polynomial_mapping_product(first_degree, second_degree, 1,
+                                       first_polynomial_coeffs[:, 2].reshape(-1, 1), second_polynomial_coeffs[:, 0].reshape(-1, 1), A2B0)
+    compute_polynomial_mapping_product(first_degree, second_degree, 1,
+                                       first_polynomial_coeffs[:, 2].reshape(-1, 1), second_polynomial_coeffs[:, 1].reshape(-1, 1), A2B1)
+
+    # Assemble the cross product from the terms
+    # NOTE: must reshape for broadcasting to convert (3, 1) to (3,)
+    product_polynomial_coeffs[:, 0] = A1B2.reshape(-1) - A2B1.reshape(-1)
+    product_polynomial_coeffs[:, 1] = A2B0.reshape(-1) - A0B2.reshape(-1)
+    product_polynomial_coeffs[:, 2] = A0B1.reshape(-1) - A1B0.reshape(-1)
 
 
 def compute_polynomial_mapping_dot_product(dimension: int, first_degree: int, second_degree: int, first_polynomial_coeffs: np.ndarray, second_polynomial_coeffs: np.ndarray, product_polynomial_coeffs: np.ndarray) -> None:
@@ -193,7 +252,7 @@ def quadratic_real_roots(quadratic_coeffs: np.ndarray, solutions: list, num_solu
 
 
 # TODO: maybe just add a type alias for A_coeffs to be... if that's supported in Python 3.11
-def polynomial_real_roots(A_coeffs: np.ndarray) -> list:
+def polynomial_real_roots(A_coeffs: np.ndarray) -> np.ndarray:
     """ Compute the real roots of a polynomial.
 
     Args:
@@ -204,7 +263,36 @@ def polynomial_real_roots(A_coeffs: np.ndarray) -> list:
     """
     # TODO: check that A_coeffs is 1D vector
 
-    pass
+    logger.info("Full coefficient vector: %s", A_coeffs)
+
+    # TODO: removes trailing coefficints by refernece... so A_coeffs is the one that has been modified!
+    reduced_coeffs: np.ndarray = remove_polynomial_trailing_coefficients(
+        A_coeffs)
+    logger.info("Reduced coefficient vector: %s", reduced_coeffs)
+
+    # TODO: whenever vector<double> used in C++ code... use NumPy ndarray???
+    # roots should be the same size as reduced_coeffs... or at least allocated to have the same space...
+    roots: np.ndarray
+
+    # TODO: fix this by returning NumPy ndarray
+    # check if reduced coeff is 0
+    if (reduced_coeffs.size == 1 and float_equal_zero(reduced_coeffs[0])):
+        return roots
+
+    # Compute the complex roots
+    solver_roots = np.roots(reduced_coeffs)
+
+    # Find the real roots (in the style of the C++ version)
+    # XXX: Involves a floating point threshold test... well... the C++ version did.
+    # Should only grab the true parts...
+    # https://stackoverflow.com/questions/28081247/print-real-roots-only-in-numpy
+    # TODO: double check w/ prof if it's OK to go about using NumPy's float checker rather than using the float checker in common.py...
+    # Because that would be redundant systems that would add bloat?
+    # TODO: utilize the tolerance value inside common.py
+    real_roots = solver_roots.real[abs(solver_roots.imag) < 1e-10]
+
+    logger.info("Real roots: %s", real_roots)
+    return real_roots
 
 
 def formatted_monomial(variable: str, degree: int) -> str:
