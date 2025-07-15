22d76f966376a35383b9805b6233d96851f8fbd2
mk39-png <113801254+mk39-png@users.noreply.github.com>
2025-07-14 19:50:46 -0500

    Conic finally working.

--- src/core/conic.py
+++ src/core/conic.py
@@ -28,36 +28,16 @@ class Conic(RationalFunction):
     # ************
     # Constructors
     # ************
-    def __init__(self, type=ConicType.UNKNOWN):
+    def __init__(self, type=ConicType.UNKNOWN, *args, **kwargs):
+        super().__init__(2, 2, *args, **kwargs)
+
         self.m_type = type
         assert self.__is_valid()
-
-    # TODO: fix this whole parent class intitialization thing
-    @classmethod
-    def from_numerator_denominator(cls, numerator_coeffs: np.ndarray, denominator_coeffs: np.ndarray):
-        super().__init__(cls.from_real_line(2, 2, numerator_coeffs, denominator_coeffs))
-
-        # TODO: double check that this is in fact passing in value UNKNOWN
-        return cls(ConicType.UNKNOWN)
-
-    @classmethod
-    def from_numerator_denominator_type(cls, numerator_coeffs: np.ndarray, denominator_coeffs: np.ndarray, m_type: ConicType):
-        super().from_real_line(2, 2, numerator_coeffs, denominator_coeffs)
-        return cls(m_type)
-
-    @classmethod
-    def from_numerator_denominator_domain(cls, numerator_coeffs: np.ndarray, denominator_coeffs: np.ndarray, domain: Interval):
-        super().from_interval(2, 2, numerator_coeffs, denominator_coeffs, domain)
-        return cls(ConicType.UNKNOWN)
-
-    @classmethod
-    def from_numerator_denominator_domain_type(cls, numerator_coeffs: np.ndarray, denominator_coeffs: np.ndarray, domain: Interval, m_type: ConicType):
-        super().from_interval(2, 2, numerator_coeffs, denominator_coeffs, domain)
-        return cls(m_type)
 
     # ******
     # PUBLIC
     # ******
+
     def get_type(self) -> ConicType:
         """
         @brief Get the type (e.g. hyperbola, line, etc.) of the conic.
@@ -71,19 +51,18 @@ class Conic(RationalFunction):
         """
         assert rotation.shape == (2, 2)
         assert translation.shape == (1, 2)
-        P_rot_coeffs: np.ndarray = self.get_numerators * \
-            rotation + self.get_denominator * translation
+        P_rot_coeffs: np.ndarray = self.get_numerators @ \
+            rotation + self.get_denominator @ translation
         assert P_rot_coeffs.shape == (3, 2)
         self.set_numerators(P_rot_coeffs)
 
-    def pullback_quadratic_function(self, dimension: int, F_coeffs: np.ndarray, pullback_function: RationalFunction):
+    def pullback_quadratic_function(self, dimension: int, F_coeffs: np.ndarray) -> RationalFunction:
         # TODO: change function to return pullback_function?
         assert F_coeffs.shape == (6, dimension)
-        assert pullback_function.get_degree == 4
-        assert pullback_function.get_dimension == dimension
+        assert F_coeffs.dtype == np.float64
 
         logger.info("Pulling back conic by quadratic function %s",
-                    formatted_bivariate_quadratic_mapping(self.m_dimension, F_coeffs))
+                    formatted_bivariate_quadratic_mapping(dimension, F_coeffs))
 
         # Separate the individual polynomial coefficients from the rational function
         P_coeffs = self.get_numerators
@@ -97,7 +76,11 @@ class Conic(RationalFunction):
         assert Q_coeffs.shape == (3, 1)
 
         logger.info("u function before pullback: (%s)/(%s)",
-                    formatted_polynomial(self.get_degree, self.get_dimension, u_coeffs), formatted_polynomial(self.get_degree, self.get_dimension, Q_coeffs))
+                    u_coeffs, Q_coeffs)
+
+        logger.info("v function before pullback: (%s)/(%s)",
+                    v_coeffs, Q_coeffs)
+        # formatted_polynomial(self.get_degree, self.get_dimension, u_coeffs), formatted_polynomial(self.get_degree, self.get_dimension, Q_coeffs))
 
         # Compute (homogenized) polynomial coefficients for the quadratic terms
         QQ_coeffs = np.ndarray(shape=(5, 1))
@@ -121,29 +104,38 @@ class Conic(RationalFunction):
 
         # Combine quadratic monomial functions into a matrix
         # NOTE: need to flatten the NP matrices into vectors from (5,1) to (5,) shape for broadcasting to work
+        # NOTE: Also transposing with .T to be shape (5,6) rather than (6,5)
         monomial_coeffs = np.array([QQ_coeffs.flatten(),
                                     Qu_coeffs.flatten(),
                                     Qv_coeffs.flatten(),
                                     uv_coeffs.flatten(),
                                     uu_coeffs.flatten(),
-                                    vv_coeffs.flatten()])
+                                    vv_coeffs.flatten()]).T
         assert monomial_coeffs.shape == (5, 6)
         logger.info("Monomial coefficients matrix:\n%s", monomial_coeffs)
 
         # Compute the pulled back rational function numerator
         logger.info("Quadratic coefficient matrix:\n%s", F_coeffs)
-        pullback_coeffs = monomial_coeffs * F_coeffs
-        assert pullback_coeffs.shape == (5, self.m_dimension)
+        pullback_coeffs = monomial_coeffs @ F_coeffs
+        assert pullback_coeffs.shape == (5, dimension)
 
         logger.info("Pullback numerator: %s",
-                    formatted_polynomial(self.get_degree, self.get_dimension, pullback_coeffs))
+                    pullback_coeffs)
+        # formatted_polynomial(self.get_degree, self.get_dimension, pullback_coeffs))
         logger.info("Pullback denominator: %s",
-                    formatted_polynomial(self.get_degree, self.get_dimension, QQ_coeffs))
+                    QQ_coeffs)
+        # formatted_polynomial(self.get_degree, self.get_dimension, QQ_coeffs))
 
         # XXX: domain() is in the C++. Meanwhile, Python code here uses self.m_domain... may cause problems.
         # XXX: specifically, just be wary of the getter that's used in the C++ code.
-        pullback_function = RationalFunction.from_interval(
-            4, self.m_dimension, pullback_coeffs, QQ_coeffs, self.m_domain)
+        # NOTE: below RationalFunction is made from interval
+        pullback_function = RationalFunction(
+            4, dimension, pullback_coeffs, QQ_coeffs, self.m_domain)
+
+        # Redundant checks just to be extra safe
+        assert pullback_function.get_degree == 4
+        assert pullback_function.get_dimension == dimension
+        return pullback_function
 
     def __is_valid(self):
         if (self.m_numerator_coeffs.shape[1] == 0):
