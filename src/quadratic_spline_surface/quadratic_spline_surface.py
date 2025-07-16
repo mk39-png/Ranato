880faa57769e054c74bf6b804795661099f34251
mk39-png <113801254+mk39-png@users.noreply.github.com>
2025-07-16 10:39:33 -0500

    Adjusting logic for quadratic_spline_surface_patch constructor

---
+++ src/quadratic_spline_surface/quadratic_spline_surface.py
@@ -0,0 +1,5 @@
+
+from src.core.common import *
+from src.core.evaluate_surface_normal import generate_quadratic_surface_normal_coeffs
+from src.quadratic_spline_surface.position_data import *
+from src.quadratic_spline_surface.quadratic_spline_surface_patch
