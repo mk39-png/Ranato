68aa3c41da6527093d61ffb58d47e032bbd5bfa0
mk39-png <113801254+mk39-png@users.noreply.github.com>
2025-10-26 18:44:26 -0500

    chore: updated Blender addon manifest

--- ranato/__init__.py
+++ ranato/__init__.py
@@ -1,30 +1,40 @@
+# bl_info = {
+#     "name": "Ranato",
+#     "author": "Kevin Ha",
+#     "version": (0, 0, 1, 'Alpha'),
+#     "blender": (4, 5, 0),
+#     "description": "Blender implementation of Algebraic Smooth Occluding Contours paper",
+#     "location": "Render -> Ranato",
+#     "warning": "",
+#     "doc_url": "",
+#     "tracker_url": "",
+#     "category": "Render"
+# }
+
 import os
 import subprocess
 import sys
 from typing import Any
 
-from . import operators, panels
-
-bl_info: dict[str, Any] = {
-    "name": "Ranato",
-    "description": "Blender implementation of Algebraic Smooth Occluding Contours paper",
-    "author": "Kevin Ha",
-    "version": (0, 0, 1, 'Alpha'),
-    "blender": (4, 5, 0),
-    "category": "Render"
-}
-
+from .panels import OBJECT_PT_ranato_panel
+from .preferences import OBJECT_OT_addon_preferences, RanatoPreferences
+from .search_mesh import OBJECT_OT_search_mesh_operator
 
 # https://blenderartists.org/t/can-i-install-pandas-or-other-modules-into-blenders-python/1375122
+# NOTE: must try and install these packages before running any one of the other modules
 # NOTE: the below only works for Windows systems
 try:
     import bpy
+    import cholespy
     import igl
     import matplotlib
     import mpmath as mp
+    import pytest
 except ImportError:
     # Blender Python interpreter location
     python_exe: str = os.path.join(sys.prefix, 'bin', 'python.exe')
+
+    # FIXME: fix the whole install of cholespy... which is not working for some reason...
 
     # This is the folder that Blender automatically holds its installed addons
     requirements_path: str = os.path.join(os.path.dirname(__file__), "requirements.txt")
@@ -37,25 +47,8 @@ except ImportError:
     subprocess.call([python_exe, "-m", "pip", "install", "-r", requirements_path])
 
 
-def register() -> None:
-    """
-    Registering files associated with the addon for Blender to recognize.
-    Called whenever addon is activated.
-    """
-    panels.register()
-    operators.register()
-
-
-def unregister() -> None:
-    """
-    Unregistering files associated with the addon for Blender to recognize.
-    Called whenever addon is deactivated.
-    """
-    panels.unregister()
-    operators.unregister()
-
-
-# This allows you to run the script directly from Blender's Text editor
-# to test the add-on without having to install it.
-if __name__ == "__main__":
-    register()
+classes: list = [OBJECT_OT_search_mesh_operator,
+                 OBJECT_OT_addon_preferences,
+                 OBJECT_PT_ranato_panel,
+                 RanatoPreferences,]
+register, unregister = bpy.utils.register_classes_factory(classes)
