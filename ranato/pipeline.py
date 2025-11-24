2d17bd6249d16b815352f3f251520cd1f059ac97
mk39-png <113801254+mk39-png@users.noreply.github.com>
2025-11-23 18:35:19 -0600

    fix: addon projection camera calculation now multiplies with world matrix

--- ranato/pipeline.py
+++ ranato/pipeline.py
@@ -15,23 +15,22 @@ from .algebraic_contours.exec.generate_algebraic_contours import \
     generate_algebraic_contours
 from .common import DIRECTORY_TEMP, FILEPATH_UV_UNWRAPPER
 
+# def call_uv_unwrapper() -> None:
+#     """
+#     Calls the uv_unwrapper algorithm with specified commands.
+#     Also calls to activate the Conda environment if that has not been done already.
+#     """
+#     # Call sys subprocess to execute python script in another file.
+#     file_path_conda = bpy.context.preferences.addons[__package__].preferences.file_path_conda
 
-def call_uv_unwrapper() -> None:
-    """
-    Calls the uv_unwrapper algorithm with specified commands.
-    Also calls to activate the Conda environment if that has not been done already.
-    """
-    # Call sys subprocess to execute python script in another file.
-    file_path_conda = bpy.context.preferences.addons[__package__].preferences.file_path_conda
-
-    subprocess.run([file_path_conda,
-                    FILEPATH_UV_UNWRAPPER,
-                    "--input", DIRECTORY_TEMP,
-                    "--fname", "temp.obj",
-                    "--output", DIRECTORY_TEMP,
-                    "--output_type", "param",
-                    "--output_format", "obj"],
-                   check=False)
+#     subprocess.run([file_path_conda,
+#                     FILEPATH_UV_UNWRAPPER,
+#                     "--input", DIRECTORY_TEMP,
+#                     "--fname", "temp.obj",
+#                     "--output", DIRECTORY_TEMP,
+#                     "--output_type", "param",
+#                     "--output_format", "obj"],
+#                    check=False)
 
 
 def get_projection_matrix(context: bpy.types.Context) -> Matrix:
@@ -49,11 +48,24 @@ def get_projection_matrix(context: bpy.types.Context) -> Matrix:
     scene: bpy.types.Scene | None = context.scene
     render: bpy.types.RenderSettings = scene.render
     camera: bpy.types.Object | None = scene.camera
-    # print(camera.data.view_frame())
 
-    modelview_matrix: Any | Matrix = camera.matrix_world.inverted()
-    print(modelview_matrix)
-    # TODO: print out the camera matrix as well for debugging...
+    print("CAMERA FRAME: \n", camera.data.view_frame(scene=scene))
+    print("CAMERA MAT WORLD: \n", camera.matrix_world)
+    print("CAMERA BASIS: \n", camera.matrix_basis)
+
+    modelview_matrix = Matrix(
+        ((3.346195029192903236e-01, 3.371676824530447491e-03,
+          -1.931632359086701556e-01, 3.634420946012050652e-02),
+         (0.000000000000000000e+00, 3.863264718173403112e-01,
+          6.743353649060895849e-03, -4.317130968153862908e-02),
+         (1.931926600865509769e-01, -5.839915566789230343e-03,
+          3.345685387482297268e-01, 1.937049982654145852e+00),
+         (0.000000000000000000e+00, 0.000000000000000000e+00,
+          0.000000000000000000e+00, 1.000000000000000000e+00)))
+
+    # camera.matrix_world = modelview_matrix
+    # modelview_matrix: Any | Matrix = camera.matrix_world.inverted()
+    print("CAMERA MODELVIEW MAT: \n", modelview_matrix)
 
     projection_matrix: Matrix = camera.calc_matrix_camera(
         depsgraph,
@@ -63,7 +75,10 @@ def get_projection_matrix(context: bpy.types.Context) -> Matrix:
         scale_y=render.pixel_aspect_y,
     )
 
-    return projection_matrix
+    print("PROJECTION MAT: \n", projection_matrix)
+    print("RESULT MAT: \n", projection_matrix @ modelview_matrix)
+
+    return projection_matrix @ camera.matrix_world
 
 
 class OBJECT_OT_pipeline(bpy.types.Operator):
@@ -73,19 +88,21 @@ class OBJECT_OT_pipeline(bpy.types.Operator):
     """
     bl_idname: str = "object.pipeline"
     bl_label: str = "Contours Pipeline"
-    # bl_property = "pipeline"
 
     def execute(self, context: bpy.types.Context) -> set:
         """
         Execute the operator.
         Runs the contour calculation pipeline.
         """
+        #
+        # DEPRECATED: rely solely on the fact that the mesh has been UV unwrapped already
+        #
         # Now with these two in the temp folder, we can call the uv_unwrapper
-        self.report({'INFO'}, "Calling UV unwrapper...")
+        # self.report({'INFO'}, "Calling UV unwrapper...")
         # call_uv_unwrapper()
 
         # Now, with the UV unwrapped mesh, we can now call the main program for
-        # processing the whole mesh... from another folder.
+        # processing the whole mesh.
         projection_matrix: Matrix = get_projection_matrix(context)
         self.report({'INFO'}, "Generating algebraic contours...")
         generate_algebraic_contours(projection_matrix)
